1c8d0cdc05e88032b75dae742e8a4239b692bf4a
saintabomination <saint.abomination@protonmail.com>
2022-05-02 21:42:40 +0200

    Adding MaterialUI icons

--- src/components/Post/Post.component.tsx
+++ src/components/Post/Post.component.tsx
@@ -1,13 +1,19 @@
+import { Avatar } from '@mui/material';
+
 import './Post.styles.css';
 
 const Post = (): JSX.Element =>
   (
-    <div>
+    <div className="post">
+      <Avatar
+        className="post__avatar"
+        alt="user.name1"
+      />
       <h3>Author name</h3>
       <div className="post__image"></div>
       <div className="post__caption">
         <p>
-          Lorem mollitia ipsa officia obcaecati provident hic molestiae Iste ullam non totam non modi Placeat officia vel natus molestias est.
+          <strong>user.name1</strong> Lorem mollitia ipsa officia obcaecati provident hic molestiae Iste ullam non totam non modi Placeat officia vel natus molestias est.
         </p>
       </div>
     </div>
