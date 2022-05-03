740d084d51639b5f192b84304db259a750a6c577
saintabomination <saint.abomination@protonmail.com>
2022-05-03 19:33:11 +0200

    Adding Firebase to the project

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+import firebaseConfig from './firebase-config';
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+const storage = getStorage(firebaseApp);
+
+export { db, auth, storage };
